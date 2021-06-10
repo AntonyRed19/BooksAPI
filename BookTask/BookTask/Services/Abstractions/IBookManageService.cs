636c19647dc68ad8d052e9b49b2bec1a7b15f824
DESKTOP-GCCS405\Antony <anton.redy19@gmail.com>
2021-06-10 19:47:49 +0300

    added Services

---
+++ BookTask/BookTask/Services/Abstractions/IBookManageService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookTask.Entities;
+
+namespace BookTask.Services.Abstractions
+{
+    public interface IBookManageService
+    {
+        Task<bool> CreateAsync(int id, string title, string description);
+        Task<bool> UpdateAsync(int id, string title, string description);
+        Task<bool> DeleteAsync(int id);
+        Task<BookEntity> GetByIdAsync(int id);
+    }
+}
---
+++ BookTask/BookTask/Services/BookManageService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookTask.BookProviders.Abstractions;
+using BookTask.Entities;
+using BookTask.Services.Abstractions;
+
+namespace BookTask.Services
+{
+    public class BookManageService : IBookManageService
+    {
+        private readonly IBookProvider _bookProvider;
+        public BookManageService(IBookProvider bookProvider)
+        {
+            _bookProvider = bookProvider;
+        }
+
+        public async Task<bool> CreateAsync(int id, string title, string description)
+        {
+            return await _bookProvider.CreateAsync(id, title, description);
+        }
+
+        public async Task<bool> DeleteAsync(int id)
+        {
+            return await _bookProvider.DeleteAsync(id);
+        }
+
+        public async Task<BookEntity> GetByIdAsync(int id)
+        {
+            return await _bookProvider.GetByIdAsync(id);
+        }
+
+        public async Task<bool> UpdateAsync(int id, string title, string description)
+        {
+            return await _bookProvider.UpdateAsync(id, title, description);
+        }
+    }
+}
